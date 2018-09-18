ec097d37c63cf4d83e2f23bee4d1586f5c00826e
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-09-18 09:47:06 +0200

    error handling added

--- datarequesting.py
+++ datarequesting.py
@@ -1,7 +1,5 @@
 import os
 import requests
-import pprint
-import json
 
 class DataRequesting(object):
     def __init__(self):
@@ -11,7 +9,9 @@ class DataRequesting(object):
         city = str(city.capitalize())
         current_weather = {}
         weather_request = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}'.format(city) + ',{}'.format(country_symbol) + '&APPID={}'.format(self.auth)).json()
-        for weather in weather_request["list"][0:20]:
-            current_weather[weather["dt_txt"]] = int(weather["main"]["temp"])-273
-        return str(weather_request["country"]) + str(weather_request["city"]) + str(current_weather).replace("{", "").replace("}","")
-
+        try:
+            for weather in weather_request["list"][0:20]:
+                current_weather[weather["dt_txt"]] = int(weather["main"]["temp"])-273
+            return str(weather_request["city"]).replace("{", "").replace("}", "") + str(current_weather).replace("{", "").replace("}", "")
+        except KeyError:
+            print('No city found')
--- infobot.py
+++ infobot.py
@@ -1,27 +1,34 @@
-import telepot
 import os
-from datarequesting import DataRequesting
-import pprint
 import time
+import telepot
+from datarequesting import DataRequesting
 
 class Infobot(object):
     def __init__(self):
         self.bot = telepot.Bot(os.environ['telepot_auth'])
         self.methods = ["Weather"]
-    
+ 
     def weather_sender(self, city, country_symbol):
         weather = DataRequesting()
         weather_info = weather.current_weather(city=city, country_symbol=country_symbol)
-        self.bot.sendMessage(os.environ['telepot_id'], weather_info)
-    
+        try:
+            self.bot.sendMessage(os.environ['telepot_id'], weather_info)
+        except:
+            self.bot.sendMessage(os.environ['telepot_id'], "city not found")
+
     def message_handler(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         print(content_type, chat_type, chat_id)
+        options = "At this moment bot allows you to check weather with command: weather {city} {country or country_symbol}"
         if content_type == 'text':
+            self.bot.sendMessage(chat_id, options)
             self.bot.sendMessage(chat_id, 'you asked for : {}'.format(msg['text']))
-            if 'weather' in msg['text']:
-                splitted_msg = msg['text'].split(' ')
-                self.weather_sender(splitted_msg[1], splitted_msg[2])
+            if 'weather' or 'Weather' in msg['text']:
+                try:
+                    splitted_msg = msg['text'].split(' ')
+                    self.weather_sender(splitted_msg[1], splitted_msg[2])
+                except IndexError:
+                    self.bot.sendMessage(chat_id, "unexpected error, not enough arguments".format(msg['text']))
 
     def test_receiver(self):
         self.bot.message_loop(self.message_handler)
@@ -31,7 +38,7 @@ def main():
     info = Infobot()
     info.test_receiver()
     while True:
-        time.sleep(10)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
